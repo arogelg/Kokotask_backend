4b4adbd7324236912fbbd1c0e6171c2702475b44
Alejandro Rogel <alex_rogelgarcia1@baylor.edu>
2024-08-18 19:05:54 -0500

    Modifying the FrontEnd URL

--- dist/config/cors.js
+++ dist/config/cors.js
@@ -4,16 +4,20 @@ exports.corsConfig = void 0;
 exports.corsConfig = {
     origin: function (origin, callback) {
         const whitelist = [process.env.FRONTEND_URL];
-        if (process.argv[2] === '--api') {
-            whitelist.push(undefined);
+
+        if (process.argv.includes('--api')) {
+            whitelist.push(undefined); // Allow undefined origins if running with --api
         }
-        if (whitelist.includes(origin)) {
+
+        // Allow all origins if FRONTEND_URL is not set (for testing purposes)
+        if (!process.env.FRONTEND_URL || whitelist.includes(origin)) {
             callback(null, true);
-        }
-        else {
+        } else {
             console.error(`CORS error: origin ${origin} not allowed by CORS`);
             callback(new Error('Error by CORS'));
         }
-    }
+    },
+    credentials: true, // Allow cookies and other credentials if needed
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify the allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Specify the allowed headers
 };
-//# sourceMappingURL=cors.js.map
